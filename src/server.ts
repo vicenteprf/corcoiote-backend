19f453ad8929532f43116e222482d437a74568fa
Jota Xavier <c.juniorfelixx@gmail.com>
2026-06-23 21:57:04 -0300

    feat: atualizar servidor e adicionar resposta com filtro de entrada

--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,28 @@
 import http from "node:http";
 
+const users = [
+	{
+		name: "Messi",
+		status: true,
+	},
+	{
+		name: "C. Ronaldo",
+		status: true,
+	},
+	{
+		name: "Wesley",
+		status: false,
+	},
+];
+
 http
-	.createServer(() => {
-		console.log("Hello bb ;)");
+	.createServer((req, res) => {
+		if (req.url === "/users" && req.method === "GET") {
+			res.writeHead(200, { "content-type": "application/json" });
+			res.end(JSON.stringify(users));
+			return;
+		}
+		res.writeHead(404, { "content-type": "text/plain" });
+		res.end("not found");
 	})
 	.listen(Number(process.env.PORT));
