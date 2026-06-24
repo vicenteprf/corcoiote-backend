93d084cb342f02f2eb887e4ea79974b5bc9c04bd
Jota Xavier <c.juniorfelixx@gmail.com>
2026-06-23 22:23:19 -0300

    feat: adicionado express e rotas

--- src/server.ts
+++ src/server.ts
@@ -1,28 +1,24 @@
-import http from "node:http";
+import express from "express";
+
+const app = express();
+
+app.use(express.json());
 
 const users = [
 	{
-		name: "Messi",
+		name: "Cristiano Ronaldo",
 		status: true,
 	},
 	{
-		name: "C. Ronaldo",
+		name: "Lionel Messi",
 		status: true,
 	},
 	{
-		name: "Wesley",
+		name: "Neymar Júnior",
 		status: false,
 	},
 ];
 
-http
-	.createServer((req, res) => {
-		if (req.url === "/users" && req.method === "GET") {
-			res.writeHead(200, { "content-type": "application/json" });
-			res.end(JSON.stringify(users));
-			return;
-		}
-		res.writeHead(404, { "content-type": "text/plain" });
-		res.end("not found");
-	})
-	.listen(Number(process.env.PORT));
+app.get("/users", (_req, res) => {
+	res.json(users);
+});
