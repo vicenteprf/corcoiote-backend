dcd447491b2a765408e02b5cd0943d2ec5485738
Vicente Ramos <vicenteprfilho09@gmail.com>
2026-06-30 22:14:37 -0300

    refactor: mover dados simulados para diretorio dedicado

---
+++ src/mocks/customer.mock.ts
@@ -0,0 +1,22 @@
+import type { Customer } from '../types.ts';
+
+export const customers: Customer[] = [
+	{
+		id: 1,
+		name: 'Cristiano Ronaldo',
+		email: 'cris@gmail.com',
+		status: true,
+	},
+	{
+		id: 2,
+		name: 'Lionel Messi',
+		email: 'lionel@gmail.com',
+		status: true,
+	},
+	{
+		id: 3,
+		name: 'Neymar Junior',
+		email: 'Ney@gmail.com',
+		status: false,
+	},
+];
