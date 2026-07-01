8638824f556701b2b836d288e73040b288ed1a17
Jota Xavier <c.juniorfelixx@gmail.com>
2026-06-30 22:17:53 -0300

    feat: estabelecer camada de servico para recurso de  clientes

---
+++ src/services/customer.service.ts
@@ -0,0 +1,59 @@
+import { customers } from "../mocks/customer.mock.ts";
+import type { CreateCustomer, Customer, UpdateCustomer } from "../types.ts";
+
+export function findAllcustomers(): Customer[] {
+	return customers;
+}
+
+export function findCustomerById(id: number): Customer {
+	const customer = customers.find((customer) => customer.id === id);
+
+	if (!customer) {
+		throw new Error(`Clente não encontrado.`);
+	}
+
+	return customer;
+}
+
+export function insertCustomer({ name, email }: CreateCustomer): Customer {
+	const id = customers[customers.length - 1].id;
+
+	const customer: Customer = {
+		id: id + 1,
+		name,
+		email,
+		status: true,
+	};
+	customers.push(customer);
+
+	return customer;
+}
+
+export function modifyCustomer(
+	id: number,
+	{ name, email, status }: UpdateCustomer,
+) {
+	const customer = customers.find((customer) => {
+		return customer.id === id;
+	});
+
+	if (!customer) {
+		throw new Error("Cliente não encontrado");
+	}
+	if (name) customer.name = name;
+	if (email) customer.email = email;
+	if (status !== undefined) customer.status = status;
+
+	return customer;
+}
+
+export function removeCustomer(id: number): void {
+	const index = customers.findIndex((customer) => {
+		return customer.id === id;
+	});
+	if (index === -1) {
+		throw new Error("Cliente não encontrado.");
+	}
+
+	customers.splice(index, 1);
+}
