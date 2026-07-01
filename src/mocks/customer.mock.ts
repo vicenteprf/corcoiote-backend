0b522b746f3aed74c0679d2b8db2cecce065bce7
Jota Xavier <c.juniorfelixx@gmail.com>
2026-06-30 22:14:37 -0300

    refactor: mover dados simulados para diretorio dedicado

---
+++ src/mocks/customer.mock.ts
@@ -0,0 +1,22 @@
+import type { Customer } from "../types.ts";
+
+export const customers: Customer[] = [
+	{
+		id: 1,
+		name: "Cristiano Ronaldo",
+		email: "cris@gmail.com",
+		status: true,
+	},
+	{
+		id: 2,
+		name: "Lionel Messi",
+		email: "lion@gmail.com",
+		status: true,
+	},
+	{
+		id: 3,
+		name: "Neymar Júnior",
+		email: "ney@gmail.com",
+		status: false,
+	},
+];
