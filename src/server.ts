a79d3132ae298c8ba6d7f1f8472e36cb06aa1a19
Vicente Ramos <vicenteprfilho09@gmail.com>
2026-07-08 17:51:05 -0300

    refactor: remove mock de teste e integra rotas oficiais no servidor

--- src/server.ts
+++ src/server.ts
@@ -1,26 +1,19 @@
 import express from 'express';
+import customerRouter from './routes/customer.routes.ts';
+import invoiceRouter from './routes/invoice.routes.ts';
 
 const app = express();
 
-const users = [
-	{
-		name: 'Cristiano Ronaldo',
-		status: true,
-	},
-	{
-		name: 'Lionel Messi',
-		status: true,
-	},
-	{
-		name: 'Neymar Junior',
-		status: false,
-	},
-];
-
 app.use(express.json());
 
-app.get('/users', (_req, res) => {
-	res.json(users);
+app.use('/customers', customerRouter);
+
+app.use('/invoices', invoiceRouter);
+
+app.use((_req, res) => {
+	res.status(404).json({
+		message: 'Not found!',
+	});
 });
 
 app.listen(Number(process.env.PORT));
